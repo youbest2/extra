d6fc1010169ceec227dff657980dd14e91fabbbc
RAJEEV RANJAN <youbest2@hotmail.com>
2019-03-10 18:06:52 +0530

    Update File_manipulatoin.c

--- File_manipulatoin.c
+++ File_manipulatoin.c
@@ -1,2 +1,5 @@
-//https://codeforwin.org/2018/01/c-program-read-and-display-file-contents.html
+// https://codeforwin.org/2018/01/c-program-read-and-display-file-contents.html
+// https://fresh2refresh.com/c-programming/c-file-handling/fscanf-fprintf-ftell-rewind-functions-c/
+// https://codeforwin.org/2018/02/c-program-find-and-replace-a-word-in-file.html
+// https://stackoverflow.com/questions/16868439/how-do-i-write-a-program-to-swap-a-character-in-the-input-file-with-a-character#
 ///Program to read a file character by character using fgetc()?
